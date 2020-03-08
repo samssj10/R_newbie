c329a9a8174ae6279c3c98a3de0f61afaa21e451
samssj10 <46166721+samssj10@users.noreply.github.com>
2020-03-08 11:33:00 +0530

    outlier analysis added

--- missing_val.r
+++ missing_val.r
@@ -54,4 +54,60 @@ marketing_train$custAge[is.na(marketing_train$custAge)]=mean(marketing_train$cus
 marketing_train$custAge[is.na(marketing_train$custAge)]=median(marketing_train$custAge,na.rm=T)
 marketing_train=knnImputation(marketing_train,k=5)
 
+#convert string factors into numeric factors
+for(i in 1:ncol(marketing_train))
+{
+  if(class(marketing_train[,i])=="factor")
+  {
+    marketing_train[,i]=factor(marketing_train[,i],labels=factor(1:length(levels(factor(marketing_train[,i])))))
+  }
+}
+
+##outlier analysis
+numeric_index=sapply(marketing_train,is.numeric) #selects the numeric variables
+numeric_data=marketing_train[,numeric_index]
+cnames=colnames(numeric_data)
+for (i in 1:length(cnames))
+   {
+     assign(paste0("gn",i), ggplot(aes_string(y = (cnames[i]), x = "responded"), data = subset(marketing_train))+ 
+              stat_boxplot(geom = "errorbar", width = 0.5) +
+              geom_boxplot(outlier.colour="red", fill = "grey" ,outlier.shape=18,
+                           outlier.size=1, notch=FALSE) +
+              theme(legend.position="bottom")+
+              labs(y=cnames[i],x="responded")+
+              ggtitle(paste("Box plot of responded for",cnames[i])))
+   }
+# paste and paste 0 used to concatenate strings
+# paste (…, sep = " ", collapse = NULL)
+# paste0(…, collapse = NULL)
+# paste0 is same as paste(…, sep = "", collapse = NULL)
+# labs (Modify axis, legend, and plot labels)
+
+##plotting plots together
+gridExtra::grid.arrange(gn1,gn5,gn2,ncol=3)
+gridExtra::grid.arrange(gn6,gn7,ncol=2)
+gridExtra::grid.arrange(gn8,gn9,ncol=2)
+
+#remove outliers using boxplot method
+df=marketing_train
+val=marketing_train$previous[marketing_train$previous %in% boxplot.stats(marketing_train$previous)$out] #%in% is a search operator that searches for values in rhs in lhs
+marketing_train=marketing_train[which(!marketing_train$previous %in% val),] #removes the outlier values from marketing_train$previous
+#for every pattern searched using a vector using %in% that pattern is removed from the vector
+for(i in cnames)
+{
+  val=marketing_train[,i][marketing_train[,i] %in% boxplot.stats(marketing_train[,i])$out]
+  marketing_train=marketing_train[which(!marketing_train[,i] %in% val),]
+}
+
+#replace all outliers with NA
+for(i in cnames)
+{
+  val=marketing_train[,i][marketing_train[,i] %in% boxplot.stats(marketing_train[,i])$out]
+  print(length(val))
+  marketing_train[,i][marketing_train[,i] %in% val]=NA
+}
+
+head(knnImputation(marketing_train,k=3)) #does not work due to even no of obs
+sum(complete.cases(marketing_train))
+
 
